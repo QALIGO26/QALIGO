a5fd44f00a7c8f6cf2073022326b26f9c6b9c1da
QALIGO26 <ramadhanishemshanguzo2002@gmail.com>
2025-04-21 20:57:37 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'QALIGO26', // Your name
+  ownerNumber: '0624073199', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
